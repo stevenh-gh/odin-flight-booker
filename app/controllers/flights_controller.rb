19be7a2f8e7878e8ce84e0df2f6b828bbffd5c7e
steven <steven.h@live.com>
2021-02-04 16:03:39 -0800

    update flights#index view and action
    add airport and date options class variable to flights#index
    add 4 dropdown menus to flights#index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    # @flight = Flight.new
+    @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date] }
   end
 end
