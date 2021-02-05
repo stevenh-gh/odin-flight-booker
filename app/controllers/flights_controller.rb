f1699bb5a2dadf7cea389c47b4d0c433aa079924
steven <steven.h@live.com>
2021-02-04 18:01:36 -0800

    add flight results to flights#index view modify date_options in flights controller add strong parameters in flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,14 @@
 class FlightsController < ApplicationController
   def index
     # @flight = Flight.new
+    # @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date] }
     @airport_options = Airport.all.map { |a| [a.code, a.id] }
-    @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date] }
+    @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date.strftime('%Y%m%d')] }
+
+    @flight_results = Flight.where start_id: query_params[:from_code], finish_id: query_params[:to_code]
+  end
+
+  def query_params
+    params.permit :from_code, :to_code, :num_tickets, :date, :commit
   end
 end
