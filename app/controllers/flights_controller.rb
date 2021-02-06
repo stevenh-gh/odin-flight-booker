a7cb3bacbdfc3e6069378b2982a6591582b6c5e1
steven <steven.h@live.com>
2021-02-06 14:33:15 -0800

    change form_for for form_with in flights#index view. instantiated class variable booking in flight#index action. form_with will submit to bookings#new with a get request.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    # @flight = Flight.new
-    # @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date] }
     @airport_options = Airport.all.map { |a| [a.code, a.id] }
     @date_options = Flight.all.map { |f| [f.start_date.strftime('%B %d, %Y'), f.start_date.strftime('%Y%m%d')] }
 
     @flight_results = Flight.where start_id: query_params[:from_code], finish_id: query_params[:to_code]
+    @booking = Booking.new
   end
 
   def query_params
