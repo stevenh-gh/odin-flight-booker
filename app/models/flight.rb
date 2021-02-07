0eb22befb552d81d97c8c8a669f64a49ca71810c
steven <steven.h@live.com>
2021-02-06 18:44:37 -0800

    add description attribute for radio buttons

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
 
   has_many :bookings
   has_many :passengers
+
+  def description
+    %Q(Flight \##{id}:
+       [ #{from_airport.code} → #{to_airport.code} ]
+       [ Date: #{start_date.strftime('%B %d, %Y')} ]
+       [ Time: #{start_date.strftime('%k:%M')} / #{start_date.strftime('%l:%M%P')} ]
+       [ Flight duration: #{flight_duration} ])
+  end
 end
