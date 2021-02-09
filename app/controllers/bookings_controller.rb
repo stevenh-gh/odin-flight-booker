d7d298a609eb4d60d4bcd22fa21de2c4c9f80293
steven <steven.h@live.com>
2021-02-08 20:39:54 -0800

    update bookings#new
    fill in bookings#create and bookings#show:
    bookings#create makes a new Booking instance from strong params passenger_params
    passengers_attributes in passenger_params is from bookings#new view's accepts_nested_attributes_from :passengers
    passenger instances are able to be made w/ associations just from Booking.new passenger_params (no need to build passengers manually)
    
    booking#show gets the booking from id and displays its attributes into the view.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @num_passengers = booking_params[:num_passengers]
+    @flight_id = booking_params[:flight_id]
+
+    # @passengers = []
+    # @num_passengers.to_i.times { @passengers << @booking.passengers.build }
+    @num_passengers.to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    # @booking = Booking.new flight_id: passenger_params[:flight_id], num_passengers: passenger_params[:num_passengers]
+    # @booking.passengers.build(passenger_params[:passengers_attributes])
+    # passenger_params[:num_passengers].to_i.times do
+    #   p @booking.passengers.build passenger_params[:passenger]
+    # end
+    @booking = Booking.new passenger_params
+    if @booking.save
+      redirect_to booking_path(@booking.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit :num_passengers, :flight_id
+  end
+
+  def passenger_params
+    # params.require(:booking).permit :flight_id, :num_passengers, passenger: %i[name email flight_id]
+    params.require(:booking).permit :flight_id, :num_passengers, passengers_attributes: %i[name email flight_id]
+  end
 end
